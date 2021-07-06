0dc6520b0d48229856fab53748910115d08c90ad
Michael Cicutto <Michael.cicutto@gmail.com>
2021-07-05 20:07:36 -0500

    close and open cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import classes from './HeaderCartButton';
 
 const HeaderCartButton = (props) => {
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
