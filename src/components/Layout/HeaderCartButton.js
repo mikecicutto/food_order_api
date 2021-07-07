88bd098e4dc3b475e6875488f11a810ff3f5e2a6
Michael Cicutto <Michael.cicutto@gmail.com>
2021-07-06 21:43:49 -0500

    form input update

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -16,7 +16,7 @@ const HeaderCartButton = (props) => {
             <span className={classes.icon}>
                 <CartIcon />
             </span>
-            <span>Your Cart </span>
+            <span>Your Cart</span>
             <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );        
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,7 +9,12 @@ const defaultCardState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + state.total.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
     }
     return defaultCardState;
 };
