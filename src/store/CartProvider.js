a0f3949c368f68e11e392bcaeb4bb70575ea42f2
Michael Cicutto <Michael.cicutto@gmail.com>
2021-07-06 19:36:35 -0500

    update cart

---
+++ src/store/CartProvider.js
@@ -0,0 +1,42 @@
+import { useReducer } from 'react';
+
+import CartContext from './cart-context';
+
+const defaultCardState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        
+    }
+    return defaultCardState;
+};
+
+const CartProvider = props => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCardState);
+
+    const addItemToCartHandler = item => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
+
+    const removeItemFromCartHandler = id => {
+        dispatchCartAction({type: 'REMOVE', id: id});
+    };
+
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler,
+    };
+
+    return (
+    <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+    )
+};
+
+export default CartProvider;
